d27b336f18b6d6d455695cc1f42bf94bf12d8146
Remix Workspace <>
2022-10-30 10:46:53 +0800

    solidity_learning

---
+++ 水龙头/Faucet.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+// WTF Solidity by 0xAA
+import "../ERC20/IERC20.sol";
+pragma solidity ^0.8.4;
+
+contract Faucet{
+    uint256 public allowanceA = 100;
+    address public tokenContract;
+    mapping(address => bool) public requestAddress;
+    
+    event SendToken(address indexed Receiver, uint256 indexed Amount);
+
+    constructor(address _tokenContract){
+        tokenContract = _tokenContract;
+    }
+
+    function send() external{
+        require( requestAddress[msg.sender] == false ,"The address has get!");
+        IERC20 t = IERC20(tokenContract);
+        require(t.balanceOf(address(this)) >= allowanceA,"Faucet Empty!");
+
+        t.transfer(msg.sender,allowanceA);
+        requestAddress[msg.sender] = true;
+
+        emit SendToken(msg.sender,allowanceA);
+    }
+}
